b437b87b7ab3392d1a83e6349008ef94e3cec09d
YuanFang97 <yuanfang0097@gmail.com>
2019-08-01 20:55:15 +0800

    实现侧边栏JS效果

--- js/HomePage.js
+++ js/HomePage.js
@@ -5,15 +5,45 @@ $(function () {
             let oneCss = $(".carousel-a1").css("display");
             let towCss = $(".carousel-a2").css("display");
             if (oneCss == "block" && towCss == "none") {
-                console.log("+++");
+
                 $(".carousel-a1").css("display", "none");
                 $(".carousel-a2").css("display", "block");
             }
             if (towCss == "block" && oneCss == "none") {
-                console.log("---");
                 $(".carousel-a1").css("display", "block");
                 $(".carousel-a2").css("display", "none");
             }
         }, 3000)
     }();
+    // 自动轮播图
+    $(".my-banner").edslider({
+        width:document.documentElement.clientWidth,
+        height:468,
+        interval:1000,
+        duration:500,
+        navigator:true,
+        paginator:false,
+    })
+    // 设置侧边栏的功能
+    let windowHeight = $(window).height() - 30;
+    $("#sidebar").css("height",`${windowHeight}`);
+    $(document).scroll(function(){
+        let height = $(document).scrollTop();
+        if(height >= 30){
+            $("#sidebar").css({"height":"100%","top":"0"});
+        }else{
+            $("#sidebar").css({"height":"100%","top":30-height});
+        }
+        if(height >= 60){
+            $()
+        }
+    })
+    // 侧边栏一系列动画效果
+    $("#sidebar>div").mouseenter(function(){
+        $($(this).children()[1]).stop().animate({
+            right:'40px'
+        },300)
+    }).mouseleave(function(){
+        $($(this).children()[1]).css("right","70px")
+    })
 })
